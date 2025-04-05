72cea901d12e6840fb46d4346752d24598977deb
Kedar Apte <apte.k@northeastern.edu>
2025-04-05 13:53:39 -0400

    Bug push

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,38 +1,17 @@
-/**
- * @file userRoutes.js
- * @description Routes for user registration, authentication, and search.
- * @module routes/userRoutes
- */
-
 const express = require("express");
 const {
   registerUser,
   authUser,
   allUsers,
+  getUserDomains,
 } = require("../controllers/userControllers");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-/**
- * @route GET /api/user
- * @description Get or search users by name or email.
- * @access Protected
- */
 router.route("/").get(protect, allUsers);
-
-/**
- * @route POST /api/user
- * @description Register a new user.
- * @access Public
- */
 router.route("/").post(registerUser);
-
-/**
- * @route POST /api/user/login
- * @description Authenticate a user with email and password.
- * @access Public
- */
 router.post("/login", authUser);
+router.get("/getDomains/:userId", getUserDomains)
 
 module.exports = router;
